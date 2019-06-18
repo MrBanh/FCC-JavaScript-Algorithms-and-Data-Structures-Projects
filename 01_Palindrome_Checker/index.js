f94c512c0c349605660f1221b724f827efb5c55d
DESKTOP-0P21BI7\Tony B <25971310+MrBanh@users.noreply.github.com>
2019-06-18 02:56:52 -0700

    Complete Project # 1 - Palindrome Checker

--- 01_Palindrome_Checker/index.js
+++ 01_Palindrome_Checker/index.js
@@ -1,14 +1,33 @@
 // https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker
 function palindrome(str) {
     // Remove non-alphanumeric characters (punctuation, spaces, symbols)
+    let newStr = str.replace(/\W|_/g, '');
 
     // Turn everything to lower or upper case
+    newStr = newStr.toLowerCase();
 
-    // Test if palindrome
-        // If at any point the characters do not match from both ends, return false
+    /**
+     * Function is used to test if the string is a palindrome
+     * @param {*} s     // string
+     * @param {*} low   // index to access the left side of string
+     * @param {*} high  // index to access the right side of string
+     * @returns         // True if palindrome, false otherwise
+     */
+    const isPalindrome = (s, low, high) => {
+        // Base case, exhausted the string
+        if (high <= low) {
+            return true;
+        } else if (s[low] !== s[high]) {
+            // If at any point the characters do not match from both ends, return false
+            return false;
+        } else {
+            // Recursively call function
+            return isPalindrome(s, low + 1, high - 1);
+        }
+    }
 
-    // Exhausted the whole string without returning false, so it is a palindrome
-    return true;
+    // Test if it is a palindrome and return
+    return isPalindrome(newStr, 0, newStr.length - 1);
 }
 
 module.exports = palindrome;
